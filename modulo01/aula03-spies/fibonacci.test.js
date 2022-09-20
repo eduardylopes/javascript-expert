55ff2b0e4ae03c1d52501723c8361da56e9f911f
Eduardy Lopes <eduardylopes@gmail.com>
2022-09-20 19:44:15 -0300

    modulo01 aula03 done

---
+++ modulo01/aula03-spies/fibonacci.test.js
@@ -0,0 +1,45 @@
+const Fibonacci = require('./fibonacci');
+const sinon = require('sinon');
+const assert = require('assert');
+
+// Fibonacci: o próximo valor corresponde à soma dos dois anteriores
+// dado 3 -> [0, 1, 1]
+// dado 5 -> [0, 1, 1, 2, 3]
+
+(async () => {
+  {
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+
+    for await (const i of fibonacci.execute(3)) {
+    }
+    // Algorítimo vai começar do zero!
+    const expectedCallCount = 4;
+    assert.deepStrictEqual(spy.callCount, expectedCallCount);
+  }
+
+  {
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+    const [...results] = fibonacci.execute(5);
+    // [0] input = 5, current = 0, next = 1
+    // [1] input = 4, current = 1, next = 1
+    // [1] input = 3, current = 1, next = 2
+    // [2] input = 2, current = 2, next = 3
+    // [3] input = 1, current = 3, next = 5
+    // [5] input = 0 -> STOP
+
+    console.log(results);
+
+    const { args } = spy.getCall(2);
+    const expectedResult = [0, 1, 1, 2, 3];
+    const expectedParams = Object.values({
+      input: 3,
+      current: 1,
+      next: 2,
+    });
+
+    assert.deepStrictEqual(args, expectedParams);
+    assert.deepStrictEqual(results, expectedResult);
+  }
+})();
